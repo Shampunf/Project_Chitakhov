2b14cc173e505a54d50ebb98a7b686c6393c6f29
Shampunf <romancitahov@gmail.com>
2025-09-27 12:20:10 +0300

    PEP-8

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
-num = int(input("Введите число (> 999): "))
+def main():
+    """Выводит цифру в разряде сотен числа > 999 (через try/except)."""
+    try:
+        number = int(input("Введите число (> 999): "))
+        if number <= 999:
+            print("Ошибка: число должно быть больше 999")
+            return
 
-hundreds = (num // 100) % 10
+        hundreds_digit = (number // 100) % 10
+        print("Цифра в разряде сотен:", hundreds_digit)
 
-print("Цифра в разряде сотен:", hundreds)
+    except ValueError:
+        print("Ошибка: введено не число")
+
+
+if __name__ == "__main__":
+    main()

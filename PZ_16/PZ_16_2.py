6a2b6cdb7f4ef4bf7fa7f957dc5fbd02c3a8e4d0
Shampunf <romancitahov@gmail.com>
2026-06-13 10:24:42 +0300

    t

--- PZ_16/PZ_16_2.py
+++ PZ_16/PZ_16_2.py
@@ -3,8 +3,20 @@ class Shape:
     height = 0
 class Regtangle(Shape):
     def area(self):
-        return self.weight*self.height
-class Square(Shape):
+        return self.width * self.height
+
+    def perimeter(self):
+        return 2 * (self.width + self.height)
+
+
+class Rectangle(Figure):
+    pass
+
+
+class Square(Figure):
+    def __init__(self, side):
+        super().__init__(side, side)
+
     def area(self):
         return self.weight*self.weight
     def perimeter(self):
